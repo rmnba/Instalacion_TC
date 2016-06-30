09469f8d6cd62767c4fa9118b84c1d4b53e9b6b6
iquiros <iquiros@CT-LT-00029.GrupoCT.net>
2016-06-30 12:39:37 +0200

    Añadidas librerías necesarias

--- Prueba_Instalacion_TC/src/MoreAdvApi32.java
+++ Prueba_Instalacion_TC/src/MoreAdvApi32.java
@@ -1,5 +1,33 @@
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.WString;
+import com.sun.jna.platform.win32.Advapi32;
+import com.sun.jna.platform.win32.WinBase.PROCESS_INFORMATION;
+import com.sun.jna.platform.win32.WinBase.STARTUPINFO;
 
-public interface MoreAdvApi32
+public interface MoreAdvApi32 extends Advapi32
 {
+	MoreAdvApi32 INSTANCE = (MoreAdvApi32) Native.loadLibrary("AdvApi32", MoreAdvApi32.class);
 	
+	boolean CreateProcessWithLogonW
+    (WString lpUsername,
+     WString lpDomain,
+     WString lpPassword,
+     int dwLogonFlags,
+     WString lpApplicationName,
+     WString lpCommandLine,
+     int dwCreationFlags,
+     Pointer lpEnvironment,
+     WString lpCurrentDirectory,
+     STARTUPINFO  lpStartupInfo,
+     PROCESS_INFORMATION lpProcessInfo);
+
+	public static final int LOGON_WITH_PROFILE          = 0x00000001;
+	public static final int LOGON_NETCREDENTIALS_ONLY   = 0x00000002;
+
+
+	int CREATE_NO_WINDOW            = 0x08000000;
+	int CREATE_UNICODE_ENVIRONMENT  = 0x00000400;
+	int CREATE_NEW_CONSOLE          = 0x00000010;
+	int DETACHED_PROCESS            = 0x00000008;
 }
