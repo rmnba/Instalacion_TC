1de7fc81665f57d8e1b49493cdf31753e4a5bc59
iquiros <iquiros@CT-LT-00029.GrupoCT.net>
2016-07-01 14:02:28 +0200

    No se

--- Prueba_Instalacion_TC/src/Ventana.java
+++ Prueba_Instalacion_TC/src/Ventana.java
@@ -1,10 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 //import java.io.BufferedReader;
 //import java.io.InputStream;
 //import java.io.InputStreamReader;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
